38ec71563fc2a29d426039ef781fc3855a0aa27c
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-06-01 19:42:57 +0200

    renamed all .exs to .ex

--- lib/macros/exp_say.ex
+++ lib/macros/exp_say.ex
@@ -7,16 +7,14 @@ defmodule ExpSay do
   }
 
   defmacro say({op, _, [num1, num2]}) when is_number(num1) and is_number(num2) do
-    {op_name, postix} = @op_names[op]   
+    {op_name, postfix} = @op_names[op]   
 
     quote do
       "#{unquote(op_name)} #{unquote(num1)} #{unquote(postfix)} #{unquote(num2)}"
     end
   end
 
-  defmacro say({op, _, [num, exp]}) do when is_number(num) and is_tuple(exp) do
-    
-  end
+  defmacro say({op, _, [num, exp]}) when is_number(num) and is_tuple(exp) do
     
   end
 end
